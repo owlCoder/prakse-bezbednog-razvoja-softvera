e9441179289ce43a106328fa677911b9bbb83d63
Danijel Jovanović <danijel.xda@gmail.com>
2023-10-19 20:29:01 +0200

    update auth context responses

--- app/src/contexts/AuthContext.js
+++ app/src/contexts/AuthContext.js
@@ -17,7 +17,31 @@ export function AuthProvider({ children }) {
   const [loading, setLoading] = useState(true);
 
   async function register(email, password) {
-    return await createUserWithEmailAndPassword(auth, email, password);
+    try {
+      await createUserWithEmailAndPassword(auth, email, password);
+
+      // kreiranje zapisa u firestore
+      
+
+      return JSON.stringify({"response": "OK"});
+    }
+    catch(error) {
+      if(error.code === "auth/weak-password") {
+        return JSON.stringify({"response": "Lozinka mora biti minimalne dužine 6!"});
+      }
+      else if(error.code === "auth/email-already-in-use") {
+        return JSON.stringify({"response": "Email je zauzet od strane drugog korisnika!"});
+      }
+      else if(error.code === "auth/invalid-email") {
+        return JSON.stringify({"response": "Email nije u validnom obliku!"});
+      }
+      else if(error.code === "auth/operation-not-allowed") {
+        return JSON.stringify({"response": "Registracija nije moguća!"});
+      }
+      else {
+        return JSON.stringify({"response": "Desila se nepoznata greška. Pokušajte ponovo kasnije."});
+      }
+    } 
   }
 
   async function login(email, password) {
