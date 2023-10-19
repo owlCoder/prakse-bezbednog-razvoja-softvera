db4c8e85ab2296076fae55af8e6420d51382f258
Danijel Jovanović <danijel.xda@gmail.com>
2023-10-19 16:22:08 +0200

    rework base code

--- app/src/App.js
+++ app/src/App.js
@@ -1,31 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-import React, {Component} from 'react';
-
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { apiResponse: "" };
-  }
-
-  callAPI() {
-    fetch("http://localhost:10000/testAPI")
-      .then(res => res.text())
-      .then(res => this.setState({ apiResponse: res }));
-  }
-
-  componentDidMount() {
-    this.callAPI();
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <h1 className="App-intro">{this.state.apiResponse}</h1>
-      </div>
-    );
-  }
-
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
 }
 
 export default App;
