3a2eb445a399f9c8db64d9ded7507f86997f468a
Danijel Jovanović <danijel.xda@gmail.com>
2023-10-22 16:22:57 +0200

    remove scrollbar

--- app/src/components/navigation/Navbar.js
+++ app/src/components/navigation/Navbar.js
@@ -34,7 +34,7 @@ function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
   return (
     <div>
-      <nav className="bg-gray-50 fixed top-0 w-full border-b-2 border-gray-200">
+      <nav className="bg-gray-50 fixed top-0 w-full border-b-2 border-gray-200 z-50">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex items-center justify-between h-16">
             <div className="flex items-center">
