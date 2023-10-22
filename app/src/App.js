6fe42006e7f9f0a70f6452d35df024ced509b03b
Danijel Jovanović <danijel.xda@gmail.com>
2023-10-22 18:04:41 +0200

    update profile

--- app/src/App.js
+++ app/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./contexts/AuthContext";
 import Register from "./components/accounts/Register";
 import Login from "./components/accounts/Login";
+import Profile from "./components/profile/Profile";
 import ForgotPassword from "./components/accounts/ForgotPassword";
 // @ts-ignore
 import Home from './components/main/Home';
@@ -15,6 +16,7 @@ function App() {
           <Route exact path="/register" element={<Register />} />
           <Route exact path="/login" element={<Login />} />
           <Route exact path="/forgot-password" element={<ForgotPassword />} />
+          <Route exact path="/account-settings" element={<Profile />} />
         </Routes>
       </Router>
     </AuthProvider>
--- app/src/components/main/Home.js
+++ app/src/components/main/Home.js
@@ -13,7 +13,6 @@ export default function Home() {
   return (
     <main>
       <Navbar />
-      <Login />
     </main>
   );
 }
