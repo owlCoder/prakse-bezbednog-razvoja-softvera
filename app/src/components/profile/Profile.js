ea2e9de2dd89cfb0a3e1ec19284556437f6128dc
Vuk Ognjanovic <56590132+Vukajlo01@users.noreply.github.com>
2023-10-22 18:16:12 +0200

    button fix position

--- app/src/components/profile/Profile.js
+++ app/src/components/profile/Profile.js
@@ -48,13 +48,13 @@ export default function Profile() {
                         <div className="col-span-6 sm:col-span-3">
                             <label for="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
                             <input type="email" name="email" id="email" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Email" required />
+                            <div classNameName="col-span-6 sm:col-full">
+                                <button className="text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 mt-6" type="submit">Save all</button>
+                            </div>
                         </div>         
                         <div className="col-span-6 sm:col-span-3">
                             <label for="birthday" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Birthday</label>
-                            <input type="date" name="birthday" id="birthday" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" required />
-                            <div classNameName="col-span-6 sm:col-full">
-                            <button className="text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 mt-6" type="submit">Save all</button>
-                            </div>
+                            <input type="date" name="birthday" id="birthday" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" required />                            
                         </div>
                     </div>
                 </form>
