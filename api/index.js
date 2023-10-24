04eae77983fdd23ffd22ae10c100230a15fabd5c
Danijel Jovanović <danijel.xda@gmail.com>
2023-10-24 22:25:50 +0200

    create secure state api3

--- api/index.js
+++ api/index.js
@@ -1,19 +1,69 @@
-import express from 'express';
-import cors from 'cors';
-import config from './config.js';
+const express = require('express');
+const admin = require('firebase-admin');
+const serviceAccount = require('./serviceAccount.json');
 
-// Import user API route into server
-import userRoute from './Routes/user.js';
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://oibis-ftn.firebaseio.com',
+});
 
 const app = express();
+const port = process.env.PORT || 5000;
 
-app.use(cors());
-app.use(express.json());
+var bodyParser = require('body-parser')
 
-// Routes
-/// Users route
-app.use('/api', userRoute);
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
 
-app.listen(config.port, () =>
-  console.log(`Server is live @ ${config.hostUrl}`),
-);
+// parse application/json
+app.use(bodyParser.json())
+
+// Middleware to verify Firebase Authentication token
+const verifyToken = async (req, res, next) => {
+  try {
+    const idToken = req.headers.authorization;
+    const decodedToken = await admin.auth().verifyIdToken(idToken);
+    req.user = decodedToken;
+    next();
+  } catch (error) {
+    res.status(401).json({ error: 'Unauthorized' + error });
+  }
+};
+
+// Route to fetch user data from Firestore
+app.post('/api/user', verifyToken, async (req, res) => {
+  const auth_uid = req.user.uid; // property access
+
+console.log(req.body);
+const { uid } = req.body;
+
+  // Check if the request contains the 'uid' field in the body
+  if (!uid) {
+    return res.status(400).json({ error: "Missing 'uid' field in the request body." });
+  }
+
+  // Check if the authenticated user matches the requested user's UID
+  if (uid !== auth_uid) {
+    return res.status(403).json({ error: "Unauthorized." });
+  }
+
+  try {
+    const userRef = admin.firestore().collection('users').doc(uid);
+    const userDoc = await userRef.get();
+
+    if (!userDoc.exists) {
+      res.status(404).json({ error: 'User not found' });
+    } else {
+      const userData = userDoc.data();
+      res.status(200).json(userData);
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+
+// create a server listener
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
