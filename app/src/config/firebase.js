25fa8e27dd2f37c64bcc3426af9bd390831bf062
Danijel Jovanović <danijel.xda@gmail.com>
2023-10-24 23:55:16 +0200

    make secure  env on FE

--- app/src/config/firebase.js
+++ app/src/config/firebase.js
@@ -1,16 +1,17 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from 'firebase/firestore';
+require('dotenv').config(); // Load environment variables from .env
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDlOj_1Z0eCE-Jb4q92hEewpAus0fNwRrA",
-    authDomain: "oibis-ftn.firebaseapp.com",
-    projectId: "oibis-ftn",
-    storageBucket: "oibis-ftn.appspot.com",
-    messagingSenderId: "1049634728195",
-    appId: "1:1049634728195:web:aec8a402e8cacd3d8ee136",
-    measurementId: "G-V5TPZVDY4N"
-  };
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
+};
 
 const app = initializeApp(firebaseConfig);
 
