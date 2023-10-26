2332b513125d4b1ec6486420110eb4373eb51fe7
Danijel Jovanović <danijel.xda@gmail.com>
2023-10-26 16:55:39 +0200

    update login page: add already logged in

--- app/src/components/accounts/Login.js
+++ app/src/components/accounts/Login.js
@@ -15,6 +15,12 @@ export default function Login() {
   useEffect(() => {
   }, [currentUser, navigate]);
 
+  const handleGoBack = () => {
+    // You can navigate back to the homepage using your preferred routing method
+    // Replace this with your actual routing logic
+    navigate('/');
+  };
+
   // funkcija za hendl
   async function handleFormSubmit(e) {
     e.preventDefault();
@@ -30,23 +36,37 @@ export default function Login() {
 
   return (
 
-      <div className="flex flex-col md:flex-row h-screen items-center bg-white dark:bg-gray-900 text-slate-600 dark:text-slate-100">
+    <div className="flex flex-col md:flex-row h-screen items-center bg-white dark:bg-gray-900 text-slate-600 dark:text-slate-100">
 
-        {/* Image */}
-        <div className="bg-white dark:bg-gray-900 hidden lg:block w-full md:w-1/2 xl:w-2/3 h-screen">
-          <ImagePicker />
-        </div>
+      {/* Image */}
+      <div className="bg-white dark:bg-gray-900 hidden lg:block w-full md:w-1/2 xl:w-2/3 h-screen">
+        <ImagePicker />
+      </div>
 
-        {/* Content */}
-        <div className="relative w-full md:max-w-md lg:max-w-full md:mx-auto md:w-1/2 xl:w-1/3 h-screen px-6 lg:px-16 xl:px-12
+      {/* Content */}
+      <div className="relative w-full md:max-w-md lg:max-w-full md:mx-auto md:w-1/2 xl:w-1/3 h-screen px-6 lg:px-16 xl:px-12
           flex items-center justify-center">
 
-          <div className="w-full h-100">
+        <div className="w-full h-100">
 
-            {/* Content Title */}
-            <h1 className="text-xl md:text-2xl font-bold leading-tight mt-12">Log In</h1>
+          <h1 className="text-xl md:text-2xl font-bold leading-tight mt-12">
+            Log In
+          </h1>
 
-            {/* Form */}
+          {currentUser ? (
+            // Show this message when the user is already logged in
+            <div>
+              <p className="mt-6 text-green-500 font-semibold text-xl">
+                You are already logged in.
+              </p>
+              <button
+                onClick={handleGoBack}
+                className="w-full text-white bg-blue-700 hover:bg-blue-800 font-semibold rounded-lg px-4 py-3 mt-6"
+              >
+                Go Back to Homepage
+              </button>
+            </div>
+          ) : (
             <form className="mt-6" onSubmit={handleFormSubmit}>
 
               {/* Mail */}
@@ -76,13 +96,12 @@ export default function Login() {
               <div>
                 <button type="submit" className="flex flex-row items-center justify-center w-full text-white bg-blue-700 hover:bg-blue-800 font-semibold rounded-lg
                   px-4 py-3 mt-6">{loading ? <div><svg aria-hidden="true" role="status" className="inline w-4 h-4 mr-3 text-white animate-spin" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="#E5E7EB" />
-                  <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentColor" />
+                    <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="#E5E7EB" />
+                    <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentColor" />
                   </svg></div> : ""}
-                    LOG IN
+                  LOG IN
                 </button>
               </div>
-
               <hr className="my-6 border-slate-500 w-full" />
 
               {/* Create Account Link */}
@@ -95,11 +114,10 @@ export default function Login() {
                   <a href="/" className="text-gray-900 text-5xl -mt-3 -ml-1 font-normal">‹</a>
                 </div>
               </a>
-
             </form>
-          </div>
+          )}
         </div>
-
-    </div>
-  );
-}
+      </div>
+      </div>
+    );
+    }
