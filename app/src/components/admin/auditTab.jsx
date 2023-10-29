9cfe88defced09e4b15075e8de4e9afda3774641
Danijel Jovanović <danijel.xda@gmail.com>
2023-10-29 19:52:17 +0100

    get data from audit

--- app/src/components/admin/auditTab.jsx
+++ app/src/components/admin/auditTab.jsx
@@ -1,8 +1,56 @@
 import React from 'react';
-import { FaInfoCircle, FaExclamationTriangle, FaExclamationCircle } from 'react-icons/fa';
+import { useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { useAuth } from "../../contexts/AuthContext";
+import axios from "axios";
+import LoadingSpinner from '../loading/loading';
 
 const AuditLog = () => {
-    return (
+    const navigate = useNavigate();
+    const { currentUser } = useAuth();
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        setLoading(true);
+
+        const fetchData = async () => {
+            if (!currentUser) {
+                navigate("/login");
+                return;
+            }
+
+            try {
+                const token = await currentUser.getIdToken();
+                const response = await axios.post(
+                    global.APIEndpoint + "/api/audit/get",
+                    {
+                        uid: currentUser.uid
+                    },
+                    {
+                        headers: {
+                            Authorization: `${token}`,
+                            "Content-Type": "application/json",
+                        },
+                    }
+                );
+
+                setData(response.data.payload);
+                setLoading(false);
+
+                if (response.status !== 200)
+                    navigate('/' + response.status.toString());
+            } catch (error) {
+                navigate('/403')
+            }
+        }
+
+        fetchData();
+    }, [currentUser, navigate]);
+
+    return loading === true ? (
+        <LoadingSpinner />
+    ) : data !== null(
         <div className="p-4 bg-white dark:bg-slate-800 bg-opacity-60 rounded-lg shadow-lg mx-6 my-4">
             <p className="text-gray-700 dark:text-gray-200 mb-6 ml-1 my-2 text-center font-medium" style={{ fontSize: 17.5 }}>
                 The audit log contains a record of different types of messages, including information messages, warning messages, and error messages, along with their timestamps. <br />
@@ -15,6 +63,9 @@ const AuditLog = () => {
                                 Evidence Time
                             </th>
                             <th scope="col" class="px-6 py-3">
+                                Type
+                            </th>
+                            <th scope="col" class="px-6 py-3">
                                 Message
                             </th>
                         </tr>
@@ -22,24 +73,22 @@ const AuditLog = () => {
                     <tbody>
                         <tr class="bg-white border-1 border-b-gray-950 dark:bg-gray-900 dark:border-gray-700">
                             <td class="px-6 py-4 text-gray-900 whitespace-nowrap dark:text-white">
-                            Apple MacBook Pro 17"
+                                Apple MacBook Pro 17"
                             </td>
-                            <td class="px-6 py-4 flex">
-                                <FaInfoCircle className='inline mr-2 text-primary-400' /> 
-                                
-                                <FaExclamationTriangle className='inline mr-2 text-yellow-500' /> 
-
-                                <span className='inline mr-2 text-rose-700 text-xl'></span>
-                                "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."
-"There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain..."
+                            <td class="px-6 py-4">
+                                <p style={{ fontSize: 19 }} className='text-primary-500 uppercase font-medium'>Informative</p>
+                                <p style={{ fontSize: 19 }} className='text-yellow-600 uppercase font-medium'>Warning</p>
+                                <p style={{ fontSize: 19 }} className='text-red-700 uppercase font-medium'>Error</p>
+                            </td>
+                            <td class="px-6 py-4">
+                                Laptop
                             </td>
                         </tr>
                     </tbody>
 
                 </table>
             </div>
-        </div>
-    );
+        </div>)
 };
 
 export default AuditLog;
