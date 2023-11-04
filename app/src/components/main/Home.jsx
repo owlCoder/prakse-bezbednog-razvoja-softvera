a5d86976552ca7a80c5ebc5939c6e6a077000f6b
Vuk Ognjanovic <56590132+Vukajlo01@users.noreply.github.com>
2023-11-04 17:13:52 +0100

    order Home

--- app/src/components/main/Home.jsx
+++ app/src/components/main/Home.jsx
@@ -19,27 +19,9 @@ export default function Home() {
       {open ? <Popup data={data} closePopup={() => setOpen(false)} /> : <div></div>}
       <Navbar />
 
-      <section>
-        {/* Heading */}
-        <div className="flex flex-col items-center space-y-3 text-center p-2 mt-16 bg-gray-700">
-          <h1 className="font-bold text-3xl">Best seller grocery near you</h1>
-          <p className="text-xl">We provide best quality &amp; fresh items near your location</p>
-        </div>
-
-        {/* Items Container */}
-        <div className="dark:bg-slate-700 flex flex-col p-6 m-3 bg-gray-50 rounded-2xl md:rounded-none shadow-2xl md:flex-row md:space-y-0 md:space-x-10 md:m-0 md:p-16">
-
-          {/* Grid Container */}
-          <div className="grid gap-4 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 mx-auto">
-
-            <Item data={{ name: "Name1", val: 5 }} openPopup={openPopup} />
-            <Item data={{ name: "Name2", val: 10.1 }} openPopup={openPopup} />
-
-          </div>
-        </div>
-      </section>
-
-      <section className="mt-10">
+      <HeroSection />
+      
+      <section className="mt-40">
           
         {/* !-- Section Container */}
         <div className="flex justify-center items-center flex-col md:flex-row md:space-x-12 md:space-y-0 space-y-12 mx-10"> 
@@ -127,9 +109,28 @@ export default function Home() {
 
         </div>
 
-
       </section>
-      
+
+      <section>
+        {/* Heading */}
+        <div className="flex flex-col items-center space-y-3 text-center p-2 mt-16 bg-gray-700">
+          <h1 className="font-bold text-3xl">Best seller grocery near you</h1>
+          <p className="text-xl">We provide best quality &amp; fresh items near your location</p>
+        </div>
+
+        {/* Items Container */}
+        <div className="dark:bg-slate-700 flex flex-col p-6 m-3 bg-gray-50 rounded-2xl md:rounded-none shadow-2xl md:flex-row md:space-y-0 md:space-x-10 md:m-0 md:p-16">
+
+          {/* Grid Container */}
+          <div className="grid gap-4 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 mx-auto">
+
+            <Item data={{ name: "Name1", val: 5 }} openPopup={openPopup} />
+            <Item data={{ name: "Name2", val: 10.1 }} openPopup={openPopup} />
+
+          </div>
+        </div>
+      </section>
+
     </main>
   );
 }
