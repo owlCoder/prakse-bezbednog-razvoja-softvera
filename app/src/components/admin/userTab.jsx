6b7c998a14d69a33cef2995924fdc3e0e4d4d392
Danijel Jovanović <danijel.xda@gmail.com>
2023-11-05 15:55:10 +0100

    Users tab: remove email field for edit

--- app/src/components/admin/userTab.jsx
+++ app/src/components/admin/userTab.jsx
@@ -563,19 +563,6 @@ const UsersTab = () => {
                                         </div>
                                         <div className="mb-4">
                                             <label className="block text-gray-700 dark:text-gray-300 text-sm font-medium mb-2">
-                                                Email
-                                            </label>
-                                            <input
-                                                type="email"
-                                                name="email"
-                                                required
-                                                value={editData.email || ''}
-                                                onChange={handleNewAccountInputChange}
-                                                className="w-full p-2 bg-white border-primary-800 dark:bg-slate-700 text-black dark:text-white rounded-lg shadow-md outline-none"
-                                            />
-                                        </div>
-                                        <div className="mb-4">
-                                            <label className="block text-gray-700 dark:text-gray-300 text-sm font-medium mb-2">
                                                 Birthday
                                             </label>
                                             <input
