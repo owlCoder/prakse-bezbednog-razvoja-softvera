0a16c25faea54ec1c45f51360ea132372fea6ce4
Danijel Jovanović <danijel.xda@gmail.com>
2023-11-05 21:01:44 +0100

    Home: add footer

---
+++ app/src/components/footer/Footer.jsx
@@ -0,0 +1,30 @@
+import { React } from "react";
+
+export const Footer = ({ year, title, hyperlink, hyperlinkText }) => {
+
+    return (
+        <div className="relative rounded-2xl overflow-hidden shadow-2xl">
+            {/* Image div */}
+            <div className="bg-blue-500">
+              <img className="" src="https://musicbox.co.rs/images/1.vr7_resize.jpg" alt="" />
+            </div>
+            
+            {/* Text div */}
+            <div className="max-size-sm absolute bottom-10 left-0 lg:right-1/4 right-0 py-6 px-3 text-black bg-white bg-opacity-70 dark:bg-slate-900 bg dark:bg-opacity-90 dark:text-white lg:rounded-r-2xl rounded-none">
+              <div className="flex justify-between w-full">
+                  <div className="font-normal flex flex-col items-start space-y-3">
+                    <div className="inline-block px-3 py-1 text-sm text-white bg-black rounded-full">
+                        {year}
+                    </div>
+                    <div className="flex flex-col items-start space-y-3 ">
+                      <p className="2xl:text-4xl xl:text-3xl lg:text-2xl md:text-xl text-3xl">{title}</p>
+                      <a href={hyperlink} className="bg-blue-600 hover:bg-blue-700 text-white px-3 py-2 rounded-md text-sm font-medium">
+                        {hyperlinkText}
+                      </a>
+                    </div>
+                  </div>
+              </div>
+            </div>
+          </div>    
+    );
+};
--- app/src/components/main/Home.jsx
+++ app/src/components/main/Home.jsx
@@ -3,7 +3,8 @@ import React, { useState } from "react";
 import { Popup } from "./Popup";
 import Item from "../product/Item";
 import { HeroSection } from "./Hero";
-import { Card } from "./Card";
+import { Card } from "../cards/Card";
+import { Footer } from "../footer/Footer";
 
 export default function Home() {
 
@@ -16,43 +17,47 @@ export default function Home() {
   };
 
   return (
-    <main className="dark:bg-gray-900 bg-gray-100 dark:text-white min-h-screen pb-2"> 
+    <main className="dark:bg-gray-900 bg-gray-100 dark:text-white min-h-screen pb-2">
       {open ? <Popup data={data} closePopup={() => setOpen(false)} /> : <div></div>}
       <Navbar />
-  
+
       <HeroSection />
 
-      <section className="mt-40">
-          
+      <section className="mt-40 mb-20">
+
         {/* !-- Section Container */}
-        <div className="flex justify-center items-center flex-col md:flex-row md:space-x-12 md:space-y-0 space-y-12 mx-10"> 
-            <Card year={1970} title={"boards"} hyperlink={"/products/1970"} hyperlinkText={"Explore"} />
-            <Card year={2010} title={"boards"} hyperlink={"/products/2010"} hyperlinkText={"Explore"} />
-            <Card year={2020} title={"boards"} hyperlink={"/products/2020"} hyperlinkText={"Explore"} />
+        <div className="flex justify-center items-center flex-col md:flex-row md:space-x-12 md:space-y-0 space-y-12 mx-10">
+          <Card year={1970} title={"boards"} hyperlink={"/products/1970"} hyperlinkText={"Explore"} />
+          <Card year={2010} title={"boards"} hyperlink={"/products/2010"} hyperlinkText={"Explore"} />
+          <Card year={2020} title={"boards"} hyperlink={"/products/2020"} hyperlinkText={"Explore"} />
         </div>
 
       </section>
 
+      {/* 
       <section>
-        {/* Heading */}
         <div className="flex flex-col items-center space-y-3 text-center p-2 mt-16 bg-gray-700">
           <h1 className="font-bold text-3xl">Best seller grocery near you</h1>
           <p className="text-xl">We provide best quality &amp; fresh items near your location</p>
         </div>
 
-        {/* Items Container */}
+         // Items Container 
         <div className="dark:bg-slate-700 flex flex-col p-6 m-3 bg-gray-50 rounded-2xl md:rounded-none shadow-2xl md:flex-row md:space-y-0 md:space-x-10 md:m-0 md:p-16">
 
-          {/* Grid Container */}
+          // Grid Container
           <div className="grid gap-4 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 mx-auto">
 
             <Item data={{ name: "Name1", val: 5 }} openPopup={openPopup} />
             <Item data={{ name: "Name2", val: 10.1 }} openPopup={openPopup} />
-
           </div>
         </div>
-      </section>
+      </section> 
+      */}
 
+      {/* Footer */}
+      <section className="mt-20 mb-10">
+        <Footer />
+      </section>
     </main>
   );
 }
