061616400d4fcbf39f0c852efabbc31439dff1c4
Danijel Jovanović <danijel.xda@gmail.com>
2023-11-08 19:53:42 +0100

    remove cors

--- api/index.js
+++ api/index.js
@@ -7,7 +7,21 @@ var bodyParser = require('body-parser'); // Configure app settings
 
 app.use(bodyParser.json({limit: "50mb"}));
 app.use(bodyParser.urlencoded({limit: "50mb", extended: true, parameterLimit:50000}));
-app.use(cors()); // Enable CORS policy
+//app.use(cors()); // Enable CORS policy
+
+// enable cors
+app.use((req, res, next) => {
+  const allowedOrigins = ['http://127.0.0.1:5000', 'http://localhost:5000', 'http://127.0.0.1:3000', 'http://localhost:3000', 'https://oib.vercel.app/'];
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+       res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  //res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:8020');
+  res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', true);
+  return next();
+});
 
 const userRoutes = require('./routes/userRoutes');   // Import the users routes
 const auditRoutes = require('./routes/auditRoutes'); // Import the audits routes
@@ -25,20 +39,6 @@ app.use('/api/audit', auditRoutes); // Define a base path for audit routes, e.g.
 app.use('/api/order', orderRoutes); // Define a base path for order routes, e.g., '/api/order'
 app.use('/api/product', productRoutes); // Define a base path for product routes, e.g., '/api/product'
 app.use('/api/role', rolesRoutes); // Define a base path for roles routes, e.g., '/api/role'
-
-// enable cors
-app.use((req, res, next) => {
-  const allowedOrigins = ['http://127.0.0.1:5000', 'http://localhost:5000', 'http://127.0.0.1:3000', 'http://localhost:3000', 'https://oib.vercel.app/'];
-  const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
-       res.setHeader('Access-Control-Allow-Origin', origin);
-  }
-  //res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:8020');
-  res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.header('Access-Control-Allow-Credentials', true);
-  return next();
-});
 
 app.get(['/', '/api'], (req, res) => { // Default route to show static API3 page
   res.sendFile(path.join(__dirname, 'static', 'index.html')); 
